63858e371b4f7b62ed14edb6ab438bfa8a050c7d
Muvunyi Tyson <50191551+ismatyson@users.noreply.github.com>
2022-05-29 17:45:09 +0200

    changes

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,17 @@
+import {Task} from './Task'
+
+export const Tasks: Task[] = [
+    {
+         id:1,
+         text:'Doc apponoint!',
+         day: 'May 5th',
+         remainder: true,
+    },
+    {
+         id:2,
+         text:'zoom apponoint!',
+         day: 'May 6th',
+         remainder: true,
+     }
+
+]
